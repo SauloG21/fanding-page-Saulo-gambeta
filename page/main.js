8565418ae1d465153dbe65cd66033ab90660485e
Saulo <sgambetadahora@gmail.com>
2025-04-03 21:55:22 -0300

    Add files via upload

---
+++ page/main.js
@@ -0,0 +1,40 @@
+function getProjects(){
+    const urlGitHub = 'https://api.github.com/users/SauloG21/repos' //LINK REPOSITÓRIO << CÓDIGO PARA PUXAR PROJETOS DO GITHUB https://github.com/SauloG21?tab=repositories e https://api.github.com/users/SauloG21/repos
+    var loandingElement = document.getElementById('loading')
+
+    fetch(urlGitHub, {
+        method: 'GET'
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            loadingElement.style.dysplay = 'none' 
+            console.log(response)
+            showProjects(response)
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+}
+
+//mostrar repositorios
+
+function showProjects(data){
+    var listElement = document.getElementById('my-projects-list')
+
+    for(let i = 0; i < data.length; i++)
+    {
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+
+}
+
+getProjects ()
+
+
+//1 10 08
